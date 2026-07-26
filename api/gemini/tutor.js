c2cd6647316ed2cd0f40df306dcdd85c2e951694
shuja4098 <shuja.4098@gmail.com>
2026-07-26 20:57:57 +0500

    Fix tutor api syntax error

--- api/gemini/tutor.js
+++ api/gemini/tutor.js
@@ -3,7 +3,7 @@ import { GoogleGenAI } from "@google/genai";
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({
-      error: "Method not allowed"
+      error: "Method not allowed",
     });
   }
 
@@ -15,20 +15,19 @@ export default async function handler(req, res) {
     const { prompt } = req.body;
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash"",
+      model: "gemini-2.5-flash",
       contents: prompt,
     });
 
-    res.status(200).json({
+    return res.status(200).json({
       explanation: response.text,
     });
 
   } catch (error) {
-
     console.error("Gemini Error:", error);
 
-    res.status(500).json({
-      error: String(error),
+    return res.status(500).json({
+      error: error.message,
     });
-
+  }
 }
